e3c9028ad35e8df9fbe1b4dfdde5be62d6276109
GagoPalyan <134396565+GagoPalyan@users.noreply.github.com>
2024-01-05 16:22:29 +0400

    1.1

--- components/topos/topos.js
+++ components/topos/topos.js
@@ -1,33 +1,40 @@
 const toposContent = document.getElementById("ToposContent");
 const pageTitle = document.getElementById("pageTitle");
+const backImage = document.getElementById("toposSingleTitle");
 const cartObj = [
   {
     img: "../../Images/Topos/Rectangle 223.png",
+    background: "../../Images/Topos/single/topos1.jpg",
     name: "Noravan Canyan",
     text: `Below the village Ohanavan, in the Kasakh canyon ( 40°21'29.91"N 44°23'39.23"E) are located the Ohanavan crags, with sport and trad routes from 5a to 6b+ grades. The site is 30km far from Yerevan center and is really good to go during the summer, as the stream of river Kasakh in the narrow canyon cools down the temperature saving you from the heat. When you get into the village, go through the cemetery from where the dirt road will continue down in the canyon. After 600m by the dirt road, you will see a path on your right, that will lead you to the river and in a bit more, up by the stream, you will appear in between the crags. There are fixed wires to get to the West Sector, which will also help you to get to the belays for several routes starting directly above the water.`,
   },
   {
     img: "../../Images/Topos/Rectangle 227.png",
+    background: "../../Images/Topos/single/topos2.jpg",
     name: "Hell Canyan",
     text: `Below the village Ohanavan, in the Kasakh canyon ( 40°21'29.91"N 44°23'39.23"E) are located the Ohanavan crags, with sport and trad routes from 5a to 6b+ grades. The site is 30km far from Yerevan center and is really good to go during the summer, as the stream of river Kasakh in the narrow canyon cools down the temperature saving you from the heat. When you get into the village, go through the cemetery from where the dirt road will continue down in the canyon. After 600m by the dirt road, you will see a path on your right, that will lead you to the river and in a bit more, up by the stream, you will appear in between the crags. There are fixed wires to get to the West Sector, which will also help you to get to the belays for several routes starting directly above the water.`,
   },
   {
     img: "../../Images/Topos/Rectangle 230.png",
+    background: "../../Images/Topos/single/topos3.jpg",
     name: "Yerevan",
     text: `Below the village Ohanavan, in the Kasakh canyon ( 40°21'29.91"N 44°23'39.23"E) are located the Ohanavan crags, with sport and trad routes from 5a to 6b+ grades. The site is 30km far from Yerevan center and is really good to go during the summer, as the stream of river Kasakh in the narrow canyon cools down the temperature saving you from the heat. When you get into the village, go through the cemetery from where the dirt road will continue down in the canyon. After 600m by the dirt road, you will see a path on your right, that will lead you to the river and in a bit more, up by the stream, you will appear in between the crags. There are fixed wires to get to the West Sector, which will also help you to get to the belays for several routes starting directly above the water.`,
   },
   {
     img: "../../Images/Topos/Rectangle 235.png",
+    background: "../../Images/Topos/single/topos4.jpg",
     name: "Ashtarak",
     text: `Below the village Ohanavan, in the Kasakh canyon ( 40°21'29.91"N 44°23'39.23"E) are located the Ohanavan crags, with sport and trad routes from 5a to 6b+ grades. The site is 30km far from Yerevan center and is really good to go during the summer, as the stream of river Kasakh in the narrow canyon cools down the temperature saving you from the heat. When you get into the village, go through the cemetery from where the dirt road will continue down in the canyon. After 600m by the dirt road, you will see a path on your right, that will lead you to the river and in a bit more, up by the stream, you will appear in between the crags. There are fixed wires to get to the West Sector, which will also help you to get to the belays for several routes starting directly above the water.`,
   },
   {
     img: "../../Images/Topos/Rectangle 236.png",
+    background: "../../Images/Topos/single/topos5.jpg",
     name: "Kaghsi",
     text: `Below the village Ohanavan, in the Kasakh canyon ( 40°21'29.91"N 44°23'39.23"E) are located the Ohanavan crags, with sport and trad routes from 5a to 6b+ grades. The site is 30km far from Yerevan center and is really good to go during the summer, as the stream of river Kasakh in the narrow canyon cools down the temperature saving you from the heat. When you get into the village, go through the cemetery from where the dirt road will continue down in the canyon. After 600m by the dirt road, you will see a path on your right, that will lead you to the river and in a bit more, up by the stream, you will appear in between the crags. There are fixed wires to get to the West Sector, which will also help you to get to the belays for several routes starting directly above the water.`,
   },
   {
     img: "../../Images/Topos/Rectangle 237.png",
+    background: "../../Images/Topos/single/topos6.jpg",
     name: "Ohanavan",
     text: `Below the village Ohanavan, in the Kasakh canyon ( 40°21'29.91"N 44°23'39.23"E) are located the Ohanavan crags, with sport and trad routes from 5a to 6b+ grades. The site is 30km far from Yerevan center and is really good to go during the summer, as the stream of river Kasakh in the narrow canyon cools down the temperature saving you from the heat. When you get into the village, go through the cemetery from where the dirt road will continue down in the canyon. After 600m by the dirt road, you will see a path on your right, that will lead you to the river and in a bit more, up by the stream, you will appear in between the crags. There are fixed wires to get to the West Sector, which will also help you to get to the belays for several routes starting directly above the water.`,
   },
@@ -72,6 +79,7 @@ cartObj.forEach((elem) => {
     toposContent.id = "toposSingle";
     toposContent.innerHTML = `
   <section id="toposSingleTitle">
+      <img src="${elem.background}">
       <h1>${elem.name}</h1>
   </section>
   <section id="toposSingleContent">
